b7df09b94c6e49b5fcc3efd9130d05fb7c44d2f4
Fedorov <phchen0769@icloud.com>
2024-04-16 16:55:49 +0800

    添加webrtc

---
+++ sample/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <demo-component>
+    </demo-component>
+</template>
+
+<script>
+  import Demo from './components/Demo.vue'
+
+export default {
+  name: 'App',
+    components: {
+        'demo-component': Demo
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
